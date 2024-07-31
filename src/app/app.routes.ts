4f2722a9d2d7c875acfb2715eafad0569a47ba41
Daniele Di Finizio <D.Difinizio-cons@almawave.it>
2024-07-31 12:03:43 +0200

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { MovieListComponent } from './features/movie-list/movie-list.component';
 import { MovieDetailComponent } from './features/movie-detail/movie-detail.component';
 
 export const routes: Routes = [
-    {path: '', component: MovieListComponent},
+    { path: '', redirectTo: '/movies', pathMatch: 'full' },
     {path:'movies', component: MovieListComponent},
     {path:'movies/:movieId', component: MovieDetailComponent}
 ];
